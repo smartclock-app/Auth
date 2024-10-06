10af8194318cb642dbdf1da97a11b4073a9b53b2
Danny Peak <dannypeaky@gmail.com>
2024-10-06 17:23:06 +0100

    Host config schema

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { serve } from "bun";
 import { OAuth2Client } from "google-auth-library";
 
 const isProduction = Bun.env.PRODUCTION === "true";
 
-serve({
+Bun.serve({
   port: Bun.env.PORT || 3000,
   development: !isProduction,
   async fetch(request) {
@@ -12,11 +11,18 @@ serve({
     const redirectUri = isProduction ? `${Bun.env.HOST}/callback` : "http://localhost:3000/callback";
     const scope = "https://www.googleapis.com/auth/calendar.readonly";
 
+    if (pathname === "/schema") {
+      const schema = Bun.file(import.meta.dir + "/schema.json");
+      return new Response(await schema.text(), { headers: { "Content-Type": "application/json" } });
+    }
+
     if (pathname === "/google") {
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline`;
 
       return Response.redirect(authUrl, 302);
-    } else if (pathname === "/callback") {
+    }
+
+    if (pathname === "/callback") {
       const code = new URL(request.url).searchParams.get("code");
 
       const client = new OAuth2Client({
